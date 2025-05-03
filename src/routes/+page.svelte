2e3967abc60bdc1388a9ff50a19a73964d915116
Rupsnigdha <rupsnigdhakashyap@gmail.com>
2025-05-03 13:48:09 +0530

    some fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,52 @@
 	import Scene2 from '$lib/components/Scene.svelte';
 	import Background from '$lib/assets/background.jpg';
 	import ProfilePicture from '$lib/assets/me.jpg';
+	import { Share } from '@lucide/svelte';
 </script>
 
 <div class="relative h-screen overflow-hidden">
+	<div class="absolute top-4 right-4">
+		<button
+			class="relative rounded-full p-2"
+			onclick={() => {
+				if (navigator.share) {
+					navigator
+						.share({
+							title: `Rupsnigdha's Linktree`,
+							text: '',
+							url: 'https://links.rups.fun'
+						})
+						.then(() => console.log('Successful share'))
+						.catch((error) => console.log('Error sharing', error));
+				} else {
+					console.log('Share not supported on this browser, do it the old way.');
+				}
+			}}
+		>
+			<Share />
+		</button>
+	</div>
+
+	<div
+		class="absolute bottom-0 left-1/2 z-30 flex w-full -translate-x-1/2 flex-col gap-4 p-12 pb-30"
+	>
+		<div
+			class="font-raleway relative mb-2 border-2 border-black bg-[#CA8E82] px-4 py-2 font-medium"
+		>
+			<div class="absolute top-1 -right-1 -z-10 h-full w-full bg-black"></div>
+			Portfolio
+		</div>
+		<div
+			class="font-raleway relative mb-2 border-2 border-black bg-[#CA8E82] px-4 py-2 font-medium"
+		>
+			<div class="absolute top-1 -right-1 -z-10 h-full w-full bg-black"></div>
+			Garage Workshop
+		</div>
+		<div class="font-raleway relative border-2 border-black bg-[#CA8E82] px-4 py-2 font-medium">
+			<div class="absolute top-1 -right-1 -z-10 h-full w-full bg-black"></div>
+			Github
+		</div>
+	</div>
 	<div class="absolute inset-0 z-20 h-screen">
 		<Canvas>
 			<Scene />
@@ -19,11 +62,18 @@
 		</div>
 		<div class="font-raleway text-2xl font-bold">@rupsnigdha</div>
 	</div>
-	<div class="absolute inset-0 -z-10 h-screen w-screen">
-		<img
-			src={Background}
-			alt=""
-			class="absolute inset-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover"
-		/>
-	</div>
+
+	<div class="absolute inset-0 -z-10 h-screen w-screen bg-[#BAE0DA]"></div>
 </div>
+
+<style>
+	.background {
+		padding: 0;
+		margin: 0;
+		background-color: #bae0da;
+		background-image: radial-gradient(circle, #f2e7dd 5px, transparent 5px);
+		background-size: 20px 20px;
+		background-position: 0 0;
+		animation: slide 10s linear infinite;
+	}
+</style>
